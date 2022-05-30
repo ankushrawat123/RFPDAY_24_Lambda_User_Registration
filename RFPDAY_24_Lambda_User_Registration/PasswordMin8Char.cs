11c9e67e6e27a8572938298eb6f9d44995f1201d
AnkushRawat <ankushrwt344@gmail.com>
2022-05-30 11:25:48 +0530

    Uploading Regex passwordMin8Char file in Uc5 branch

---
+++ RFPDAY_24_Lambda_User_Registration/PasswordMin8Char.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RFPDAY_24_Lambda_User_Registration
+{
+    public class PasswordMin8Char
+    {
+        public string RegexPassword = "^[A-Z a-z 0-9]{8,}$";
+        public void Password8Char(string pwd)
+        {
+            if (Regex.IsMatch(RegexPassword, pwd))
+                Console.WriteLine("Minimum 8 Char Password is Valid");
+            else
+                Console.WriteLine("Minimum 8 char Password is InValid");
+        }
+    }
+}
--- RFPDAY_24_Lambda_User_Registration/Program.cs
+++ RFPDAY_24_Lambda_User_Registration/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using RFPDAY_24_Lambda_User_Registration;
 Console.WriteLine("Welcome to Lambda Registration Program");
-Console.WriteLine("Enter \n1 FOR FIRSTNAME REGEX \n2 FOR LASTNAME REGEX \n3 FOR EMAIL REGEX \n4 FOR MOBILE NUMBER REGEX");
+Console.WriteLine("Enter \n1 FOR FIRSTNAME REGEX PROGRAM \n2 FOR LASTNAME REGEX PROGRAM \n3 FOR EMAIL REGEX PROGRAM \n4 FOR MOBILE NUMBER REGEX PROGRAM \n5 FOR PASSWORD MINIMUM 8 CHAR REGEX PROGRAM");
 int num = Convert.ToInt32(Console.ReadLine());
 switch (num)
 {
@@ -23,5 +23,10 @@ switch (num)
     case 4:
         MobileNum obj4 = new MobileNum();
         obj4.MMoblie("91 9812345605");
+        break;
+
+    case 5:
+        PasswordMin8Char obj5 = new PasswordMin8Char();
+        obj5.Password8Char("Ankushrw");
         break;
 }
